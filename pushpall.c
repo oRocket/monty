47c29b372148e185007bd4eb45e4dee2aeb2286e
benedictowusu <beneyjoy@gmail.com>
2023-10-18 09:33:11 -0700

    monty

--- pushpall.c
+++ pushpall.c
@@ -2,10 +2,10 @@
 
 /**
  * push - pushes an element onto the stack.
- * @topper: pointer to the top of the stack.
- * @val: value to operate on
+ * @stack: pointer to the top of the stack.
+ * @value: value to operate on
  */
-void push(stack_t **toppper, int val)
+void push(stack_t **stack, int value)
 {
 	stack_t *new_node = malloc(sizeof(stack_t));
 
@@ -14,24 +14,24 @@ void push(stack_t **toppper, int val)
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
-	new_node->n = val;
+	new_node->n = value;
 	new_node->prev = NULL;
-	new_node->next = *topper;
-	if (*topper != NULL)
+	new_node->next = *stack;
+	if (*stack != NULL)
 	{
-		(*topper)->prev = new_node;
+		(*stack)->prev = new_node;
 	}
-	*topper = new_node;
+	*stack = new_node;
 }
 
 /**
  * pall - Prints all the values on the stack, starting from the top.
- * @topper: A pointer to the top of the stack.
+ * @stack: A pointer to the top of the stack.
  * @line_number: The current line number in the Monty bytecode file.
  */
-void pall(stack_t **topper, unsigned int line_number)
+void pall(stack_t **stack, unsigned int line_number)
 {
-	stack_t *current = *topper;
+	stack_t *current = *stack;
 
 	(void)line_number;
 
